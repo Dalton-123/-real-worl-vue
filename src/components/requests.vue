67768d891858cb4f963eaf0460afb34aa879ba0c
Dalton <dsmith27@my.ncu.edu.jm>
2019-07-08 09:07:06 -0500

    frens

--- src/components/requests.vue
+++ src/components/requests.vue
@@ -1,40 +1,42 @@
 <template>
   <div>
-    <a class="" uk-toggle="target: #friends"
-      ><i class="fa fa-user-plus">requests</i></a
-    >
+   <div>
+     <a class="" uk-toggle="target: #friends"
+     ><i class="fa fa-user-plus">requests</i><span style="position: absolute;top:-7px;font-size: 0.7em;border-radius: 25px;color:green;">{{requests.length}}</span></a
+     >
 
-    <div id="friends" uk-offcanvas="overlay: true">
-      <div class="uk-offcanvas-bar">
-        <button class="uk-offcanvas-close" type="button" uk-close></button>
+     <div id="friends" uk-offcanvas="overlay: true">
+       <div class="uk-offcanvas-bar">
+         <button class="uk-offcanvas-close" type="button" uk-close></button>
 
-        <h3 v-if="requests.length !=0">Friend Requests</h3>
-          <div v-else><h3 >You have no requests</h3>
-              <span style='font-size:200px;'>&#128577;</span></div>
-        <div
-          v-for="request in requests"
-          :key="request.requester"
-          class="uk-flex"
-        >
-          <img
-            class="uk-border-circle"
-            width="40"
-            height="40"
-            :src="request.image"
-          />
+         <h3 v-if="requests.length !=0">Friend Requests</h3>
+         <div v-else><h3 >You have no requests</h3>
+           <span style='font-size:200px;'>&#128577;</span></div>
+         <div
+                 v-for="request in requests"
+                 :key="request.requester"
+                 class="uk-flex"
+         >
+           <img
+                   class="uk-border-circle"
+                   width="40"
+                   height="40"
+                   :src="request.image"
+           />
 
-          <span>{{ request.name[0]}}</span>
-            <button v-if="!request.status" @click="confirm(request.request_id)">
-              <i class="fa fa-user-plus">confirm</i>
-            </button>
-            <span v-else style="color: #00c7ff">{{msg}}</span>
-        </div>
-      </div>
-    </div>
+           <span>{{ request.name[0]}}</span>
+           <button  @click="confirm(request.request_id,request.name[0])">
+             <i class="fa fa-user-plus">confirm</i>
+           </button>
+         </div>
+       </div>
+     </div>
+   </div>
   </div>
 </template>
 
 <script>
+  import UIkit from "uikit";
 import firebase from "firebase";
 import db from "@/firebase/init";
 export default {
@@ -43,23 +45,26 @@ export default {
     return {
       id: firebase.auth().currentUser.uid,
       requests: [],
-      msg: "you are frens ",
       status: null
     };
   },
   methods: {
-    confirm(id) {
+    confirm(id,name) {
       db.collection("friendships")
         .doc(id)
         .update({
           status: 1
-        });
-      window.location.reload()
+        }).then(()=>{
+        UIkit.notification("Ypu are now frens with" + ' ' + name)
+      }).then(()=>{
+        window.location.reload()
+      })
+
     }
   },
   created() {
     db.collection("friendships")
-      .where("user_requested", "==", this.id)
+      .where("user_requested", "==", this.id).where('status','==',null)
       .onSnapshot(querySnapshot => {
         querySnapshot.docChanges().forEach(change => {
           if (change.type === "added") {
